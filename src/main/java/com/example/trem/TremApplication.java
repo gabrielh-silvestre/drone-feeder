bd73b0be025a832135e6af6ef61a50a31fc6884e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-17 10:02:59 -0300

    start project

---
+++ src/main/java/com/example/trem/TremApplication.java
@@ -0,0 +1,13 @@
+package com.example.trem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TremApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TremApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trem/TremApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TremApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
