b86dddaf230cf82b060e0304b3988619f637a9f9
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-18 17:52:13 -0300

    test: make controllers test more readable
    - remove non-used import of TestDroneUseCase

--- src/test/java/com/example/trem/infra/api/controller/TestDroneController.java
+++ src/test/java/com/example/trem/infra/api/controller/TestDroneController.java
@@ -3,6 +3,8 @@ package com.example.trem.infra.api.controller;
 import com.example.trem.domain.drone.entity.Drone;
 import com.example.trem.domain.drone.factory.DroneFactory;
 import com.example.trem.infra.repositories.drone.DroneRepository;
+import com.example.trem.useCase.drone.dto.CreateDroneDto;
+import com.example.trem.useCase.drone.dto.UpdateDroneDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -26,63 +30,96 @@ public class TestDroneController {
   @Autowired
   private DroneRepository droneRepository;
 
+  private final Iterable<Drone> drones = List.of(new Drone[]{
+          DroneFactory.create("Drone 1", 1.0, 1.0),
+          DroneFactory.create("Drone 2", 2.0, 2.0),
+          DroneFactory.create("Drone 3", 3.0, 3.0)
+  });
+
+  private CreateDroneDto generateCreateDto() {
+    Drone drone = drones.iterator().next();
+
+    CreateDroneDto dto = new CreateDroneDto();
+    dto.setName(drone.getName());
+    dto.setLatitude(drone.getLatitude());
+    dto.setLongitude(drone.getLongitude());
+
+    return dto;
+  }
+
+  private UpdateDroneDto generateUpdateDto() {
+    Drone drone = drones.iterator().next();
+
+    UpdateDroneDto dto = new UpdateDroneDto();
+    dto.setName(drone.getName());
+    dto.setLatitude(drone.getLatitude());
+    dto.setLongitude(drone.getLongitude());
+
+    return dto;
+  }
+
   @Test
-  @DisplayName("should create a drone (POST /drones)")
+  @DisplayName("1 - should create a drone (POST /drones)")
   public void testCreateDrone() throws Exception {
-    Drone newDrone = DroneFactory.create("Drone 1", 1.0, 1.0);
+    CreateDroneDto newDrone = this.generateCreateDto();
+
     mockMvc.perform(post("/drones")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(new ObjectMapper().writeValueAsString(newDrone)))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(new ObjectMapper().writeValueAsString(newDrone)))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(jsonPath("$.name").value("Drone 1"))
             .andExpect(status().isCreated());
   }
 
   @Test
-  @DisplayName("should update a drone (PUT /drones/{id})")
+  @DisplayName("2 - should update a drone (PUT /drones/{id})")
   public void testUpdateDrone() throws Exception {
-//    Drone newDrone = DroneFactory.create("Drone 1", 1.0, 1.0);
-//
-//    droneRepository.save(DroneEntityMapper.toEntity(newDrone));
-//    newDrone.rename("Drone 2");
-//
-//    mockMvc.perform(put("/drones/{id}", newDrone.getId())
-//            .contentType(MediaType.APPLICATION_JSON)
-//            .content(new ObjectMapper().writeValueAsString(newDrone)))
-//            .andExpect(status().isOk());
-  }
+    Drone drone = drones.iterator().next();
+    droneRepository.saveAll(this.drones);
 
-  @Test
-  @DisplayName("should delete a drone (DELETE /drones/{id})")
-  public void testDeleteDrone() throws Exception {
-//    Drone newDrone = DroneFactory.create("Drone 1", 1.0, 1.0);
-//    var saved = droneRepository.save(DroneEntityMapper.toEntity(newDrone));
-//
-//    mockMvc.perform(delete("/drones/{id}", saved.getId())
-//            .contentType(MediaType.APPLICATION_JSON)
-//            .content(new ObjectMapper().writeValueAsString(newDrone)))
-//            .andExpect(status().isNoContent());
-  }
+    drone.rename("Drone 2");
 
-  @Test
-  @DisplayName("should get all drones (GET /drones)")
-  public void testGetAllDrones() throws Exception {
-    mockMvc.perform(get("/drones")
-            .contentType(MediaType.APPLICATION_JSON))
+    mockMvc.perform(put("/drones/{id}", drone.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(new ObjectMapper().writeValueAsString(drone)))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.name").value("Drone 2"))
+            .andExpect(status().isOk());
+  }
+
+//  @Test
+//  @DisplayName("3 - should delete a drone (DELETE /drones/{id})")
+//  public void testDeleteDrone() throws Exception {
+//    Drone drone = drones.iterator().next();
+//    droneRepository.saveAll(this.drones);
+//
+//    mockMvc.perform(delete("/drones/{id}", drone.getId().toString()))
+//            .andExpect(status().isNoContent());
+//  }
+
+  @Test
+  @DisplayName("4 - should get all drones (GET /drones)")
+  public void testGetAllDrones() throws Exception {
+    droneRepository.saveAll(this.drones);
+
+    mockMvc.perform(get("/drones")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$[0].name").value("Drone 1"))
             .andExpect(status().isOk());
   }
 
   @Test
-  @DisplayName("should get a drone (GET /drones/{id})")
+  @DisplayName("5 - should get a drone (GET /drones/{id})")
   public void testGetDrone() throws Exception {
-//    Drone newDrone = DroneFactory.create("Drone 1", 1.0, 1.0);
-//
-//    droneRepository.save(DroneEntityMapper.toEntity(newDrone));
-//
-//    mockMvc.perform(get("/drones/{id}", newDrone.getId())
-//            .contentType(MediaType.APPLICATION_JSON))
-//            .andExpect(status().isOk());
+    Drone drone = drones.iterator().next();
+    droneRepository.saveAll(this.drones);
+
+    mockMvc.perform(get("/drones/{id}", drone.getId())
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.name").value("Drone 1"))
+            .andExpect(status().isOk());
   }
 
 }
--- src/test/java/com/example/trem/useCase/TestDroneUseCase.java
+++ src/test/java/com/example/trem/useCase/TestDroneUseCase.java
@@ -12,7 +12,6 @@ import com.example.trem.useCase.drone.dto.CreateDroneDto;
 import com.example.trem.useCase.drone.dto.DroneDto;
 import com.example.trem.useCase.drone.dto.UpdateDroneDto;
 import com.example.trem.useCase.shared.exception.NotFoundException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
