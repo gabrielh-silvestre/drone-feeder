3501129612688b19c41741126c7a64e5b2756742
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-19 00:30:53 -0300

    test: use test profile in integration tests

--- src/test/java/com/example/trem/TremApplicationTests.java
+++ src/test/java/com/example/trem/TremApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.trem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TremApplicationTests {
 
 	@Test
